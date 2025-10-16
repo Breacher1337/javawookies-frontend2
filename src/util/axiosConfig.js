2187185c5327c8ed23577e8fd4af175613e5b45e
Elaijah <elaijahangelo@gmail.com>
2025-10-17 05:30:04 +0800

    update api link

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {BASE_URL} from "./apiEndpoints.js";
 
 const axiosConfig = axios.create({
-    baseURL: BASE_URL,
+    baseURL: `${BASE_URL}/api`,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
